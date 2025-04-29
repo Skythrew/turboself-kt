8c5f14548e199c3af6f132ec5fe11b40e59d9b16
Maël GUERIN <mael.guerin@murena.io>
2025-04-29 20:12:33 +0200

    feat(core): add base endpoints

---
+++ library/src/commonMain/kotlin/core/routes.kt
@@ -0,0 +1,7 @@
+package core
+
+const val BASE_URL = "https://api-rest-prod.incb.fr/api"
+
+// Authentication endpoints
+const val AUTH_SIGNUP = "$BASE_URL/v1/creation-comptes"
+const val AUTH_LOGIN = "$BASE_URL/v1/auth/login"
